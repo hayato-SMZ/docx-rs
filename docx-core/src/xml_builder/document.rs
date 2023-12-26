e68b0e79c0bb21227b6209006cd7a635bf442cf8
hayato-Shimizu-d1g <hy-shimizu@daiichi-g.co.jp>
2023-12-26 10:25:22 +0900

    documentのattributeを追加

--- docx-core/src/xml_builder/document.rs
+++ docx-core/src/xml_builder/document.rs
@@ -14,6 +14,10 @@ impl XMLBuilder {
                     )
                     .attr("xmlns:v", "urn:schemas-microsoft-com:vml")
                     .attr(
+                        "xmlns:m",
+                        "http://schemas.openxmlformats.org/officeDocument/2006/math",
+                    )
+                    .attr(
                         "xmlns:w",
                         "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
                     )
@@ -23,8 +27,56 @@ impl XMLBuilder {
                         "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing",
                     )
                     .attr(
+                        "xmlns:wpc",
+                        "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas",
+                    )
+                    .attr(
+                        "xmlns:cx",
+                        "http://schemas.microsoft.com/office/drawing/2014/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx1",
+                        "http://schemas.microsoft.com/office/drawing/2015/9/8/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx2",
+                        "http://schemas.microsoft.com/office/drawing/2015/10/21/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx3",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/9/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx4",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/10/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx5",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/11/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx6",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/12/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx7",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/13/chartex",
+                    )
+                    .attr(
+                        "xmlns:cx8",
+                        "http://schemas.microsoft.com/office/drawing/2016/5/14/chartex",
+                    )
+                    .attr(
                         "xmlns:wps",
                         "http://schemas.microsoft.com/office/word/2010/wordprocessingShape",
+                    )
+                    .attr(
+                        "xmlns:wpi",
+                        "http://schemas.microsoft.com/office/word/2010/wordprocessingInk",
+                    )
+                    .attr(
+                        "xmlns:wne",
+                        "http://schemas.microsoft.com/office/word/2006/wordml",
                     )
                     .attr(
                         "xmlns:wpg",
@@ -45,6 +97,22 @@ impl XMLBuilder {
                     .attr(
                         "xmlns:w15",
                         "http://schemas.microsoft.com/office/word/2012/wordml",
+                    )
+                    .attr(
+                        "xmlns:aink",
+                        "http://schemas.microsoft.com/office/drawing/2016/ink",
+                    )
+                    .attr(
+                        "xmlns:am3d",
+                        "http://schemas.microsoft.com/office/drawing/2017/model3d",
+                    )
+                    .attr(
+                        "xmlns:w16cid",
+                        "http://schemas.microsoft.com/office/word/2016/wordml/cid",
+                    )
+                    .attr(
+                        "xmlns:w16se",
+                        "http://schemas.microsoft.com/office/word/2015/wordml/symex",
                     )
                     .attr("mc:Ignorable", "w14 wp14"),
             )
